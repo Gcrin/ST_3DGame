cb8ba0b7e06b5947c1496395078ddc77908df481
Gcrin <94522022+Gcrin@users.noreply.github.com>
2025-07-02 16:47:40 +0900

    게임 모드 클래스(STGameMode) 작성 및 기본 게임 모드 설정 추가

---
+++ Source/ST_3DGame/GameModes/STGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "STGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ST_3DGAME_API ASTGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
