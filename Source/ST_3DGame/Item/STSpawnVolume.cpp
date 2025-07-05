01505cd662fc2a58d2519cd341424a6460968597
Gcrin <94522022+Gcrin@users.noreply.github.com>
2025-07-06 01:54:41 +0900

    아이템 스폰 볼륨 클래스(STSpawnVolume) 및 블루프린트 추가

---
+++ Source/ST_3DGame/Item/STSpawnVolume.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "STSpawnVolume.h"
+
+#include "Components/BoxComponent.h"
+#include "Engine/World.h"
+#include "GameFramework/Actor.h"
+
+ASTSpawnVolume::ASTSpawnVolume()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Scene = CreateDefaultSubobject<USceneComponent>(TEXT("Scene"));
+	SetRootComponent(Scene);
+
+	SpawningBox = CreateDefaultSubobject<UBoxComponent>(TEXT("SpawningBox"));
+	SpawningBox->SetupAttachment(Scene);
+}
+
+FVector ASTSpawnVolume::GetRandomPointInVolume() const
+{
+	FVector BoxExtent = SpawningBox->GetScaledBoxExtent();
+	FVector BoxOrigin = SpawningBox->GetComponentLocation();
+
+	return BoxOrigin + FVector(
+		FMath::FRandRange(-BoxExtent.X, BoxExtent.X),
+		FMath::FRandRange(-BoxExtent.Y, BoxExtent.Y),
+		FMath::FRandRange(-BoxExtent.Z, BoxExtent.Z)
+	);
+}
+
+void ASTSpawnVolume::SpawnItem(TSubclassOf<AActor> ItemClass)
+{
+	if (!ItemClass) return;
+
+	GetWorld()->SpawnActor<AActor>(
+		ItemClass,
+		GetRandomPointInVolume(),
+		FRotator::ZeroRotator
+	);
+}
